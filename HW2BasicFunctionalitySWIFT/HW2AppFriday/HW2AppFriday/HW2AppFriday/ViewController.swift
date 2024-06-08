d5c5d9d57d90823744852a436dbe2b5cc2e4560d
Bashur Viachaslau <bashvime@gmail.com>
2024-06-08 23:06:20 +0300

    пытался прикрутить код к кнопке в сториборде

--- HW2BasicFunctionalitySWIFT/HW2AppFriday/HW2AppFriday/HW2AppFriday/ViewController.swift
+++ HW2BasicFunctionalitySWIFT/HW2AppFriday/HW2AppFriday/HW2AppFriday/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leibel: UILabel!
+    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var button: UIButton!
+  
+    let pn:Int = 1
+    let wt = 2
+    let sr = 3
+    let cht = 4
+    let pt = 5
+    let weeknd = 6
+
+    //я понимаю что мне нужно переменную dni прязать к UITextField! а переменную numweek привязать к UILabel! не понимаю как это сделать
+    //почему привязывая кнопку сверху пишет @IBOutlet weak var а снизу другое 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionbutton(_ sender: Any) {
+        
+        var dni = Field.text
 
+        var numweek: Int
+        switch dni {
+        case "Понедельник":
+            numweek = weeknd - pn
+        case "Вторник":
+            numweek = weeknd - wt
+        case "Среда":
+            numweek = weeknd - sr
+        case "Четверг":
+            numweek = weeknd - cht
+        case "Пятница":
+            numweek = weeknd - pt
+        default:
+            numweek = weeknd - weeknd
+        }
+        
+    }
+    
 }
 
