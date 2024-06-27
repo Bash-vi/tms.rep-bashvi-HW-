1b2bdb8525b09c5fe631415278deaf7ab49c0282
Bashur Viachaslau <bashvime@gmail.com>
2024-06-28 02:57:38 +0300

    сделал два экрана

--- HW8storyboard/HW8storyboard/HW8storyboard/ViewController.swift
+++ HW8storyboard/HW8storyboard/HW8storyboard/ViewController.swift
@@ -6,78 +6,146 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     lazy var namePageLable = UILabel()
     lazy var nextPageLable = UILabel()
-    lazy var PasswordTextField = UITextField()
-    lazy var PasswordLaibel = UILabel()
-    lazy var UserNameTextField = UITextField()
-    lazy var UserNameLable = UILabel()
-    lazy var LoginInButton = UIButton()
+    lazy var passwordTextField = UITextField()
+    lazy var passwordLaibel = UILabel()
+    lazy var userNameTextField = UITextField()
+    lazy var userNameLable = UILabel()
+    lazy var loginInButton = UIButton()
+    lazy var box = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(namePageLable)
-        view.addSubview(nextPageLable)
-        view.addSubview(PasswordTextField)
-        view.addSubview(PasswordLaibel)
-        view.addSubview(LoginInButton)
-        view.addSubview(UserNameTextField)
-        view.addSubview(UserNameLable)
+//        view.addSubview(namePageLable)
+//        view.addSubview(nextPageLable)
+//        view.addSubview(passwordTextField)
+//        view.addSubview(passwordLaibel)
+//        view.addSubview(loginInButton)
+//        view.addSubview(userNameTextField)
+//        view.addSubview(userNameLable)
+//        
+//        namePageLable.text = "My Home Control"
+//        namePageLable.translatesAutoresizingMaskIntoConstraints = false
+//        namePageLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+//        namePageLable.font = .systemFont(ofSize: 35)
+//        namePageLable.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//       
+//        userNameLable.translatesAutoresizingMaskIntoConstraints = false
+//        userNameLable.text = "Username"
+//        userNameLable.bottomAnchor.constraint(equalTo: userNameTextField.topAnchor).isActive = true
+//        userNameLable.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+//        userNameLable.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+//        
+//        userNameTextField.placeholder = "Please enter username"
+//        userNameTextField.translatesAutoresizingMaskIntoConstraints = false
+//        userNameTextField.topAnchor.constraint(equalTo: namePageLable.bottomAnchor, constant: 150).isActive = true
+//        userNameTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        userNameTextField.backgroundColor = .lightGray
+//        userNameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+//        userNameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+//        userNameTextField.borderStyle = .roundedRect
+//        
+//        passwordLaibel.translatesAutoresizingMaskIntoConstraints = false
+//        passwordLaibel.text = "Password"
+//        passwordLaibel.topAnchor.constraint(equalTo: userNameTextField.bottomAnchor, constant: 20).isActive = true
+//        passwordLaibel.bottomAnchor.constraint(equalTo: passwordTextField.topAnchor).isActive = true
+//        passwordLaibel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+//        passwordLaibel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+//        
+//        passwordTextField.placeholder = "Please enter password"
+//        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+//        passwordTextField.topAnchor.constraint(equalTo: passwordLaibel.bottomAnchor, constant: 150).isActive = true
+//        passwordTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        passwordTextField.backgroundColor = .lightGray
+//        passwordTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+//        passwordTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+//        passwordTextField.borderStyle = .roundedRect
+//        
+//        loginInButton.setTitle("Log in", for: .normal)
+//        loginInButton.backgroundColor = .lightGray
+//        loginInButton.setTitleColor(.black, for: .normal)
+//        loginInButton.translatesAutoresizingMaskIntoConstraints = false
+//        loginInButton.topAnchor.constraint(equalTo: passwordLaibel.bottomAnchor, constant: 100).isActive = true
+//        loginInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        loginInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 120).isActive = true
+//        loginInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -120).isActive = true
+//        
+//        nextPageLable.translatesAutoresizingMaskIntoConstraints = false
+//        nextPageLable.text = "Register"
+//        nextPageLable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -35).isActive = true
+//        nextPageLable.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         
+        view.addSubview(box)
+        box.backgroundColor = .clear
+        box.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 130, left: 30, bottom: 90, right: 30))
+        }
+        
+        func AddSubviews(_ views: UIView...) {
+            views.forEach{
+                box.addSubview($0)
+                $0.translatesAutoresizingMaskIntoConstraints = false
+            }
+            }
+        AddSubviews(userNameLable, userNameTextField, passwordLaibel, passwordTextField, namePageLable, nextPageLable, loginInButton)
+            
         namePageLable.text = "My Home Control"
-        namePageLable.translatesAutoresizingMaskIntoConstraints = false
-        namePageLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        namePageLable.textColor = .white
         namePageLable.font = .systemFont(ofSize: 35)
-        namePageLable.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-       
-        UserNameLable.translatesAutoresizingMaskIntoConstraints = false
-        UserNameLable.text = "Username"
-        UserNameLable.bottomAnchor.constraint(equalTo: UserNameTextField.topAnchor).isActive = true
-        UserNameLable.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
-        UserNameLable.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        namePageLable.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(box.snp.top)
+        }
         
-        UserNameTextField.placeholder = "Please enter username"
-        UserNameTextField.translatesAutoresizingMaskIntoConstraints = false
-        UserNameTextField.topAnchor.constraint(equalTo: namePageLable.bottomAnchor, constant: 150).isActive = true
-        UserNameTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        UserNameTextField.backgroundColor = .lightGray
-        UserNameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
-        UserNameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        passwordTextField.placeholder = "Please enter password"
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.snp.makeConstraints{
+            $0.center.equalTo(view)
+            $0.left.equalToSuperview()
+            $0.right.equalToSuperview()
+        }
         
-        PasswordLaibel.translatesAutoresizingMaskIntoConstraints = false
-        PasswordLaibel.text = "Password"
-        PasswordLaibel.topAnchor.constraint(equalTo: UserNameTextField.bottomAnchor, constant: 20).isActive = true
-        PasswordLaibel.bottomAnchor.constraint(equalTo: PasswordTextField.topAnchor).isActive = true
-        PasswordLaibel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
-        PasswordLaibel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        passwordLaibel.text = "Password"
+        passwordLaibel.snp.makeConstraints {
+            $0.bottom.equalTo(passwordTextField.snp.top).offset(-2)
+            $0.left.equalToSuperview()
+        }
+        passwordLaibel.textColor = .white
         
-        PasswordTextField.placeholder = "Please enter password"
-        PasswordTextField.translatesAutoresizingMaskIntoConstraints = false
-        PasswordTextField.topAnchor.constraint(equalTo: PasswordLaibel.bottomAnchor, constant: 150).isActive = true
-        PasswordTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        PasswordTextField.backgroundColor = .lightGray
-        PasswordTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
-        PasswordTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        userNameTextField.placeholder = "Please enter username"
+        userNameTextField.borderStyle = .roundedRect
+        userNameTextField.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.left.right.equalToSuperview()
+            $0.bottom.equalTo(passwordLaibel.snp.top).offset(-25)
+        }
         
-        LoginInButton.setTitle("Log in", for: .normal)
-        LoginInButton.backgroundColor = .lightGray
-        LoginInButton.setTitleColor(.black, for: .normal)
-        LoginInButton.translatesAutoresizingMaskIntoConstraints = false
-        LoginInButton.topAnchor.constraint(equalTo: PasswordLaibel.bottomAnchor, constant: 100).isActive = true
-        LoginInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        LoginInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 120).isActive = true
-        LoginInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -120).isActive = true
+        userNameLable.text = "Username"
+        userNameLable.snp.makeConstraints {
+            $0.bottom.equalTo(userNameTextField.snp.top).offset(-2)
+            $0.left.equalToSuperview()
+        }
+        userNameLable.textColor = .white
         
-        nextPageLable.translatesAutoresizingMaskIntoConstraints = false
         nextPageLable.text = "Register"
-        nextPageLable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -35).isActive = true
-        nextPageLable.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        nextPageLable.textColor = .white
+        nextPageLable.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        loginInButton.setTitle("Log in", for: .normal)
+        loginInButton.backgroundColor = .white
+        loginInButton.setTitleColor(.black, for: .normal)
+        loginInButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(80)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(150)
+        }
         
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
